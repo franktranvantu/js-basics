d35375fba17ed1c399a75e9baa640ad422f76bcb
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-18 17:27:55 +0700

    Update

---
+++ final/js/transform.js
@@ -0,0 +1,5 @@
+console.log(multiline);
+const passphrase = 'I Have Spoken';
+console.log(passphrase.toUpperCase());
+console.log(passphrase.length);
+console.log(passphrase);
--- start/js/strings.js
+++ start/js/strings.js
@@ -1,1 +1,6 @@
-console.log("Hello, world!");
+const htmlSnippet = '<h1 class="headline">My Headline</h1>';
+const message = 'I\'m a programmer!';
+
+const multiline = "Hello, students. \
+Welcome to JavaScript Basics. \
+I hope you learn a lot!";
